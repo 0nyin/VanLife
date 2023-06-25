9cbcc78cd5cccd2a41182f548d765c9d3f8b6ac7
Anyalewechi Onyinyechi <106473945+0nyin@users.noreply.github.com>
2023-06-25 20:12:54 +0100

    adding files and gitignore

---
+++ REACT-JOB/React-job/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import About from "./Component/pages/About"
+import Home from "./Component/pages/Home"
+import "./App.css"
+
+
+function App() {
+
+  return (
+   <BrowserRouter>
+   <header>
+   <Link className="site-logo" to="/">#VanLife</Link>
+    <nav>
+      <Link to="/about">About</Link>
+    </nav>
+   </header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
+
+
